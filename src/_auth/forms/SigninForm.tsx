c6b2266e2c82738d446c0d8376bf2fcd954dfd35
luiskkkkkkkkkkkkkk <80022664+leetpro473@users.noreply.github.com>
2024-09-07 15:52:23 -0300

    Update SigninForm.tsx

--- src/_auth/forms/SigninForm.tsx
+++ src/_auth/forms/SigninForm.tsx
@@ -1,4 +1,4 @@
-import * as z from "zod";
+timport * as z from "zod";
 import { useForm } from "react-hook-form";
 import { zodResolver } from "@hookform/resolvers/zod";
 import { Link, useNavigate } from "react-router-dom";
