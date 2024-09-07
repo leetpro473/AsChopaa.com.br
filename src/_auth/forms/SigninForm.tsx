805f36795142861f74f8b5ced0e8dae7f3e86b37
luiskkkkkkkkkkkkkk <80022664+leetpro473@users.noreply.github.com>
2024-09-07 15:48:49 -0300

    Update SigninForm.tsx

--- src/_auth/forms/SigninForm.tsx
+++ src/_auth/forms/SigninForm.tsx
@@ -54,7 +54,7 @@ const SigninForm = () => {
   return (
     <Form {...form}>
       <div className="sm:w-420 flex-center flex-col">
-        <img src="/assets/images/logo.svg" alt="logo" />
+        <img src="https://imgur.com/bgfX6qX.png" alt="logo" />
 
         <h2 className="h3-bold md:h2-bold pt-5 sm:pt-12">
         Faça login na sua conta
